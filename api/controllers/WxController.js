24ef3aea4d01c6029aecfe8e6c7546b85c69c1bd
cupdir <cupdir@gmail.com>
2019-08-08 12:03:00 +0800

    zxc

--- api/controllers/WxController.js
+++ api/controllers/WxController.js
@@ -4,13 +4,25 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+const sha1 = require('sha1');
 module.exports = {
 
     // 验证服务器token
     token : function( req, res ){
-        console.log( req )
-        res.ok();
+      const token = '79f5845cf694d73b632d3134d4902bed';
+      const signature = req.signature;
+      const nonce = req.nonce;
+      const timestamp = req.timestamp;
+      const echostr = req.echostr;
+      let str = [token, timestamp, nonce].sort().join('');
+      let sha = sha1(str);
+      console.log(sha, signature);
+      if(sha == signature){
+        // this.body = echostr;
+      } else {
+        // this.body = 'wrong';
+      }
+      res.ok();
     }
 
 };
