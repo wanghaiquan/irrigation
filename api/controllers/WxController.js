5aa9ca4c72767bde31086f550791a2417b4abfc1
cupdir <cupdir@gmail.com>
2019-08-08 12:22:55 +0800

    asd

--- api/controllers/WxController.js
+++ api/controllers/WxController.js
@@ -10,13 +10,13 @@ module.exports = {
     // 验证服务器token
     token : function( req, res ){
       const token = '79f5845cf694d73b632d3134d4902bed';
-      const signature = req.signature;
-      const nonce = req.nonce;
-      const timestamp = req.timestamp;
-      const echostr = req.echostr;
+      const signature = req.query.signature;
+      const nonce = req.query.nonce;
+      const timestamp = req.query.timestamp;
+      const echostr = req.query.echostr;
       let str = [token, timestamp, nonce].sort().join('');
       let sha = sha1(str);
-      console.log(sha, signature);
+      console.log(signature,sha, signature);
       if(sha == signature){
         console.log( echostr )
         return res.status(200).send(echostr);
