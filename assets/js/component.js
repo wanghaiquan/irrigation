83e03673d8a0817966d483685a2eb9d5b3ad9bff
cupdir <cupdir@gmail.com>
2019-09-28 08:21:07 +0800

    asd

--- assets/js/component.js
+++ assets/js/component.js
@@ -1,21 +1,44 @@
 $(function() {
+  var options = {
+        connectTimeout: 4000, // 超时时间
+        // 认证信息
+        clientId: 'smart-websocket'
+  }
   var client = mqtt.connect('ws://192.144.190.105:8083/mqtt') // you add a ws:// url here
-  client.subscribe("mqtt/light")
+
+
+
+
+  client.on('connect', (e) => {
+      console.log('成功连接服务器')
+
+      // 订阅一个主题
+      client.subscribe('OUT/DEVICE/cupdir/0009/smart', { qos: 1 }, (error) => {
+          if (!error) {
+              console.log('订阅成功')
+          }
+      })
+  })
+
   client.on('message',function(topic, message){
+    console.log(message.toString())
       // $("body").toggleClass("active");
     // client.end()
   })
-  $(".power").click(function() {
-    if ($("body").hasClass('active')) {
-      $("body").removeClass("active");
-      client.publish("mqtt/light", '{ "button": "off" }')
-      $(".power").text('开灯')
+  $(".switch-button01 ").click(function() {
+    if( $(".switch-button01 input").is(":checked") == false){
+      client.publish("OUT/DEVICE/cupdir/0009/smart", '{ "button": "01" ,"cmd":"0" }')
+    }else{
+      client.publish("OUT/DEVICE/cupdir/0009/smart", '{ "button": "01" ,"cmd":"1" }')
+    }
+  })
+
+  $(".switch-button02 ").click(function() {
+    if( $(".switch-button02 input").is(":checked") == false ){
+      client.publish("OUT/DEVICE/cupdir/0009/smart", '{ "button": "02" ,"cmd":"0" }')
 
     }else{
-        console.log( 'off')
-        $(".power").text('关灯')
-        client.publish("mqtt/light", '{ "button": "on" }')
-        $("body").toggleClass("active");
+      client.publish("OUT/DEVICE/cupdir/0009/smart", '{ "button": "02" ,"cmd":"1" }')
     }
-  });
+  })
 });
